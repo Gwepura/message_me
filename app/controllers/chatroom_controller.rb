3a65f653240dcc7bc5a3439ba42c7b8500a444f9
Gwepura <aketani32@gmail.com>
2019-12-03 07:23:27 +0000

    Add root and login routes with controllers and views

---
+++ app/controllers/chatroom_controller.rb
@@ -0,0 +1,29 @@
+class ChatroomController < ApplicationController
+  def index
+    
+  end
+  
+  def new 
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def create
+    
+  end
+  
+  def update 
+    
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    
+  end
+end
